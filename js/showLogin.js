314c2445297157df7224b2a38ad89ccf7ee5682c
jawad-zh <zahraoui.jawad20@gmail.com>
2026-03-09 00:52:02 +0000

    login page

--- js/showLogin.js
+++ js/showLogin.js
@@ -1,4 +1,7 @@
- function showLogin(){
+import {authuntication} from './authu.js';
+import {showProfile} from './showProfile.js';
+
+export async function showLogin(){
     const body = document.body
     body.innerHTML = ``
     const login = `
@@ -6,12 +9,24 @@
         <h2>login</h2>
         <input type="text" name="email" id="email-username" placeholder="email or username" >
         <input type="password" id="password" placeholder="password">
-        <button  type="submit" id="loginButton" >login</button>
+        <button  id="loginButton" >login</button>
     </form>
     `
     const form = document.createElement('form')
     form.setAttribute('id','container')
     form.innerHTML = login
     body.append(form)
+    //---------------------------------------------------------
+     document.getElementById('loginButton').addEventListener('click',async (e)=>{
+        e.preventDefault()
+        const  res = await authuntication()
+        console.log('authuuu reees',res);
+        
+        if(res === 'success'){
+            showProfile()
+        }else{
+            console.log('nnnnnnnn');
+            
+        }
+    })
 }
-showLogin()
